a5e40c6554d54b3e2975d8f7a1f53514370f71bb
alijawad1511 <jawad.bukhari1511@gmail.com>
2022-04-18 16:21:13 +0500

    Done

---
+++ app/src/main/java/com/example/labtask/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.labtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword, etAge;
+    Button btnLogin;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void onClickBtnLogin(View view){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString();
+        String age = etAge.getText().toString();
+
+        if(checkValidity(email,password,age))
+        {
+
+            if(email.equals("test@umt.edu.pk") && password.equals("umt1234") && Integer.parseInt(age)<25){
+                imageView.setImageResource(R.drawable.yes);
+            }else{
+                imageView.setImageResource(R.drawable.no);
+            }
+        }
+    }
+
+    private boolean checkValidity(String email, String password, String age) {
+        boolean flag = true;
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email is required");
+            flag = false;
+        }
+
+        if (password.isEmpty()) {
+            etPassword.setError("Password is required");
+            flag = false;
+        }
+
+        if (age.isEmpty()) {
+            etAge.setError("Age is required");
+            flag = false;
+        }
+
+        return flag;
+    }
+
+    private void init(){
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etAge = findViewById(R.id.etAge);
+        btnLogin = findViewById(R.id.btnLogin);
+        imageView = findViewById(R.id.imageView);
+    }
+}
